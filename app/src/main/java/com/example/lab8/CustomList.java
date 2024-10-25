3b94300298132a637f3c9c806cc670f64d652c3c
ikwiz007 <ikhalid@ualberta.ca>
2024-10-25 17:30:16 -0600

    updated getCount function - test should pass

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -51,7 +51,7 @@ public class CustomList extends ArrayAdapter<City> {
      * @return
      */
     public int getCount(){
-        //return cities.size();
+        return cities.size();
     }
 
     /**
